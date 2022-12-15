6901b2b57665f8978df13a1e11d1d57f992e9f4f
Martin Mapoma <mapoma1112@hotmail.com>
2022-12-15 06:07:48 -0800

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers from 0 to 9.
+ * print_most_numbers - print_most_numbers from 0 to 9.
  * Do not print 2 and 4.
  * followed by a new line.
  * Return: Always 0
